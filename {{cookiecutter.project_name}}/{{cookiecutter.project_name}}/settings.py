4bc27b0939fe939d0ac51aef3b7b857f84fd67f0
Germán Moya <pbacterio@gmail.com>
2014-03-19 17:54:40 +0100

    STATIC_ROOT config with dj-paas-env

--- {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_name}}/settings.py
@@ -79,4 +79,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'wsgi/static'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+from dj_paas_env import static
+STATIC_ROOT = static.root()
